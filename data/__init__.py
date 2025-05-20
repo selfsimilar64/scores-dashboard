e417c6ee6397c61cc91e90b16b812834eeebbdb9
selfsimilar64 <mattjkelley710@gmail.com>
2025-05-20 11:53:59 -0400

    added stats table, NONE/MEDIAN/MEAN score normalization

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,4 @@
 # data/__init__.py
-from .loader import load_db, DB_PATH, TABLE_NAME
+from .loader import load_db, DB_PATH, SCORES_TABLE_NAME, STATS_TABLE_NAME
 
-__all__ = ["load_db", "DB_PATH", "TABLE_NAME"] 
+__all__ = ["load_db", "DB_PATH", "SCORES_TABLE_NAME", "STATS_TABLE_NAME"] 
